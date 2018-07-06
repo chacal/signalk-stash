658b9522354dc992942075c24b61004aa1d88cae
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-07-06 22:35:43 +0300

    Add instrument_measurement table

--- api-server/db.js
+++ api-server/db.js
@@ -7,6 +7,14 @@ const createTables = `
     timestamp TIMESTAMP WITH TIME ZONE,
     point GEOGRAPHY(Point,4326) NOT NULL,
     PRIMARY KEY (context, timestamp)
+  );
+  CREATE TABLE IF NOT EXISTS instrument_measurement (
+    context TEXT,
+    timestamp TIMESTAMP WITH TIME ZONE,
+    path TEXT NOT NULL,
+    sourceId TEXT NOT NULL,
+    value jsonb NOT NULL,
+    PRIMARY KEY (context, timestamp, path, sourceId)
   )`
 
 
