fcd42024a6c23642e24f2c0dd9bf9f709ad2b3d0
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-07-09 16:23:38 +0300

    Add runner for MQTT delta input

---
+++ delta-inputs/mqtt-runner.js
@@ -0,0 +1,8 @@
+import db from '../api-server/db'
+import SignalKDeltaWriter from '../api-server/delta-writer'
+import MqttDeltaInput from './mqtt'
+
+const MQTT_BROKER = process.env.MQTT_BROKER || 'mqtt://localhost:11883'
+
+const writer = new SignalKDeltaWriter(db)
+new MqttDeltaInput(MQTT_BROKER, writer).start()
