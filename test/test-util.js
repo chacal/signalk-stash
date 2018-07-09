951f12d9238f68a011957fd43577b3676d98e94b
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2018-07-09 16:19:00 +0300

    Add helper for polling external changes in tests

--- test/test-util.js
+++ test/test-util.js
@@ -1,5 +1,17 @@
+import BPromise from 'bluebird'
+
 export const vesselUuid =
   'urn:mrn:signalk:uuid:2204ae24-c944-5ffe-8d1d-4d411c9cea2e'
 
 export const positionFixtures = require('./data/position-fixtures.json')
 export const measurementFixtures = require('./data/measurement-fixtures.json')
+
+export function waitFor(action, predicate) {
+  return action().then(res => {
+    if (predicate(res)) {
+      return BPromise.resolve(res)
+    } else {
+      return BPromise.delay(100).then(() => waitFor(action, predicate))
+    }
+  })
+}
