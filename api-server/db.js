6ff373f5be934126b426965a3b15fe3ffa2283b6
Teppo Kurki <teppo.kurki@iki.fi>
2019-03-01 21:44:53 +0200

    chore: remove previous db structure

--- api-server/db.js
+++ api-server/db.js
@@ -9,21 +9,6 @@ const createTables = `
   CREATE EXTENSION IF NOT EXISTS "postgis";
   CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-  CREATE TABLE IF NOT EXISTS trackpoint (
-    context TEXT,
-    timestamp TIMESTAMP WITH TIME ZONE,
-    point GEOGRAPHY(Point,4326) NOT NULL,
-    PRIMARY KEY (context, timestamp)
-  );
-  CREATE TABLE IF NOT EXISTS instrument_measurement (
-    context TEXT,
-    timestamp TIMESTAMP WITH TIME ZONE,
-    path TEXT NOT NULL,
-    sourceId TEXT NOT NULL,
-    value jsonb NOT NULL,
-    PRIMARY KEY (context, timestamp, path, sourceId)
-  );
-
   CREATE TABLE IF NOT EXISTS paths (
     id SERIAL PRIMARY KEY,
     path TEXT NOT NULL,
@@ -131,30 +116,6 @@ class DB {
 
   ensureTables() {
     return this.db.query(createTables)
-  }
-
-  insertTrackpoint({ context, timestamp, longitude, latitude }) {
-    return this.db.query(
-      `
-          INSERT INTO trackpoint (context, timestamp, point)
-          VALUES ($[context], $[timestamp], st_point($[longitude], $[latitude]))
-          ON CONFLICT (context, timestamp)
-          DO UPDATE SET point = st_point($[longitude], $[latitude])
-        `,
-      { context, timestamp, longitude, latitude }
-    )
-  }
-
-  insertMeasurement({ context, timestamp, path, sourceId, value }) {
-    return this.db.query(
-      `
-          INSERT INTO instrument_measurement (context, timestamp, path, sourceId, value)
-          VALUES ($[context], $[timestamp], $[path], $[sourceId], $[value])
-          ON CONFLICT (context, timestamp, path, sourceId)
-          DO UPDATE SET value = $[value]
-        `,
-      { context, timestamp, path, sourceId, value: JSON.stringify(value) }
-    )
   }
 
   upsertAccount({ username, passwordHash, isMosquittoSuper }) {
