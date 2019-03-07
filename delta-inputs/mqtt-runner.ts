5e0ed1a1bd7ba7a99cb0273aa961f78dc5ccc121
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-03-07 21:44:07 +0200

    Verify that MQTT username and password are set

--- delta-inputs/mqtt-runner.ts
+++ delta-inputs/mqtt-runner.ts
@@ -8,18 +8,29 @@ const MQTT_BROKER = process.env.MQTT_BROKER || 'mqtt://localhost:11883'
 const MQTT_USERNAME = process.env.MQTT_USERNAME
 const MQTT_PASSWORD = process.env.MQTT_PASSWORD
 
-startMqttClient(MQTT_BROKER, MQTT_USERNAME, MQTT_PASSWORD).then(mqttClient => {
-  const writer = new SignalKDeltaWriter(db)
-  const deltaInput = new MqttDeltaInput(mqttClient, writer)
-  deltaInput.start()
-})
+if (MQTT_USERNAME === undefined || MQTT_PASSWORD === undefined) {
+  console.error('Set MQTT_USERNAME and MQTT_PASSWORD env variables properly.')
+  process.exit(1)
+} else {
+  startMqttClient(MQTT_BROKER, MQTT_USERNAME, MQTT_PASSWORD).then(
+    mqttClient => {
+      const writer = new SignalKDeltaWriter(db)
+      const deltaInput = new MqttDeltaInput(mqttClient, writer)
+      deltaInput.start()
+    }
+  )
+}
 
 process.on('unhandledRejection', error => {
   console.error('Unhandled promise exception:', error)
   process.exit(1)
 })
 
-function startMqttClient(brokerUrl, brokerUser, brokerPw) {
+function startMqttClient(
+  brokerUrl: string,
+  brokerUser: string,
+  brokerPw: string
+) {
   const client = mqtt.connect(brokerUrl, {
     username: brokerUser,
     password: brokerPw
