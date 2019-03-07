7a698b4ada2417b3796862ad4beb17fc5a232141
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-03-07 21:04:24 +0200

    Use parameter properties

--- test/TestAccount.ts
+++ test/TestAccount.ts
@@ -1,15 +1,12 @@
 import Account from '../api-server/Account'
 
 export default class TestAccount extends Account {
-  readonly password: string
-
   constructor(
     username: string,
-    password: string,
+    readonly password: string,
     passwordHash: string,
     isMqttSuperUser: boolean = false
   ) {
     super(username, passwordHash, isMqttSuperUser)
-    this.password = password
   }
 }
