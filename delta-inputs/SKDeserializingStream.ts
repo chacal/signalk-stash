fbf2f22bb24cb604a23b79c53ca707fc9ba54fd2
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-03-10 23:05:18 +0200

    Remove unneeded $source generation
    signalk-ts does this now

--- delta-inputs/SKDeserializingStream.ts
+++ delta-inputs/SKDeserializingStream.ts
@@ -1,18 +1,12 @@
-import { SKDelta, SKUpdateJSON } from '@chacal/signalk-ts'
+import { SKDelta } from '@chacal/signalk-ts'
 import { Transform, TransformCallback } from 'stream'
 
 export default class SKDeserializingStream extends Transform {
-  static fill$sourceIfNeeded(update: SKUpdateJSON) {
-    if (!update.$source && update.source) {
-      update.$source = update.source.src // TODO: Replace with a proper way to populate $source
-    }
-  }
   constructor() {
     super({ objectMode: true })
   }
 
   _transform(chunk: any, encoding: string, callback: TransformCallback): void {
-    chunk.updates.forEach(SKDeserializingStream.fill$sourceIfNeeded)
     try {
       this.push(SKDelta.fromJSON(chunk))
     } catch (e) {
