8b9e53a42a4ebc3db57525e3e99c1695135a7a11
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-03-17 09:55:21 +0200

    Get and validate context & bbox from query string

--- api-server/TrackAPI.ts
+++ api-server/TrackAPI.ts
@@ -1,13 +1,18 @@
 import { Express, Request, Response } from 'express'
+import * as Joi from 'joi'
 import stash from './db/StashDB'
+import { BBox, Coords } from './domain/Geo'
+import { Schemas, validate } from './domain/validation'
 
 export default function setupTrackAPIRoutes(app: Express) {
   app.get('/tracks', tracks)
 }
 
 function tracks(req: Request, res: Response): void {
-  // TODO: Get vessel ID from query parameters
-  stash.getVesselTracks('self').then(tracksData => {
+  const context = contextFromQuery(req)
+  const bbox = bboxFromQuery(req)
+
+  stash.getVesselTracks(context, bbox).then(tracksData => {
     res.json({
       type: 'MultiLineString',
       coordinates: tracksData.map(trackData =>
@@ -15,4 +20,29 @@ function tracks(req: Request, res: Response): void {
       )
     })
   })
+
+  function contextFromQuery(req: Request): string {
+    const schema = {
+      context: Joi.string().required()
+    }
+    return validate(req.query, schema, { allowUnknown: true }).context
+  }
+
+  function bboxFromQuery(req: Request): BBox | undefined {
+    const q = req.query
+    if (!!q.nwLat || !!q.nwLng || !!q.seLat || !!q.seLng) {
+      const bboxSchema = {
+        nwLat: Schemas.lat,
+        nwLng: Schemas.lng,
+        seLat: Schemas.lat,
+        seLng: Schemas.lng
+      }
+      validate(req.query, bboxSchema, { allowUnknown: true })
+      const nw = new Coords({ lat: q.nwLat, lng: q.nwLng })
+      const se = new Coords({ lat: q.seLat, lng: q.seLng })
+      return new BBox({ nw, se })
+    } else {
+      return undefined
+    }
+  }
 }
