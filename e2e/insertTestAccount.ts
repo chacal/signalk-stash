e3b817b6c4e37aa5c45b10e24c88ed6c00442d39
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-03-18 22:10:48 +0200

    Add ACL also for the e2e test account

--- e2e/insertTestAccount.ts
+++ e2e/insertTestAccount.ts
@@ -1,10 +1,16 @@
 import DB from '../api-server/db/StashDB'
 import { testAccount } from '../test/test-util'
+import { MqttACL, MqttACLLevel } from '../api-server/domain/Auth'
 
 console.log(testAccount)
 
 DB.ensureTables()
   .then(() => DB.upsertAccount(testAccount))
+  .then(() =>
+    DB.upsertAcl(
+      new MqttACL(testAccount.username, 'signalk/delta', MqttACLLevel.ALL)
+    )
+  )
   .then(() => {
     process.exit(0)
   })
