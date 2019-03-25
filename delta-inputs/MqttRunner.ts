c57a7278832915490a8563c9dcdeafd7033511c8
Teppo Kurki <teppo.kurki@iki.fi>
2019-03-25 22:28:20 +0200

    refactor: split mqtt-runner & MqttRunner

---
+++ delta-inputs/MqttRunner.ts
@@ -0,0 +1,55 @@
+import BPromise from 'bluebird'
+import * as mqtt from 'mqtt'
+import config from '../api-server/Config'
+import DB from '../api-server/db/StashDB'
+import SignalKDeltaWriter from '../api-server/SignalKDeltaWriter'
+import MqttDeltaInput from './MqttDeltaInput'
+
+const MQTT_BROKER =
+  process.env.MQTT_BROKER || config.mqtt.broker || 'mqtt://localhost:51883'
+const MQTT_USERNAME = process.env.MQTT_USERNAME || config.mqtt.username
+const MQTT_PASSWORD = process.env.MQTT_PASSWORD || config.mqtt.password
+
+export default class MqttRunner {
+  constructor() {
+    DB.ensureTables()
+      .then(() => {
+        if (MQTT_USERNAME === undefined || MQTT_PASSWORD === undefined) {
+          console.error(
+            'Set MQTT_USERNAME and MQTT_PASSWORD env variables properly.'
+          )
+          process.exit(1)
+        } else {
+          startMqttClient(MQTT_BROKER, MQTT_USERNAME, MQTT_PASSWORD).then(
+            mqttClient => {
+              const writer = new SignalKDeltaWriter(DB)
+              const deltaInput = new MqttDeltaInput(mqttClient, writer)
+              deltaInput.start()
+            }
+          )
+        }
+      })
+      .catch(err => {
+        console.error(err)
+        process.exit(-1)
+      })
+  }
+}
+
+function startMqttClient(
+  brokerUrl: string,
+  brokerUser: string,
+  brokerPw: string
+) {
+  const client = mqtt.connect(brokerUrl, {
+    username: brokerUser,
+    password: brokerPw
+  })
+  client.on('connect', () => console.log('Connected to MQTT server'))
+  client.on('offline', () => console.log('Disconnected from MQTT server'))
+  client.on('error', e => console.log('MQTT client error', e))
+
+  return BPromise.fromCallback(cb =>
+    client.once('connect', () => cb(null))
+  ).then(() => client)
+}
--- delta-inputs/mqtt-runner.ts
+++ delta-inputs/mqtt-runner.ts
@@ -1,56 +1,9 @@
-import BPromise from 'bluebird'
-import * as mqtt from 'mqtt'
-import config from '../api-server/Config'
-import DB from '../api-server/db/StashDB'
-import SignalKDeltaWriter from '../api-server/SignalKDeltaWriter'
-import MqttDeltaInput from './MqttDeltaInput'
-
-const MQTT_BROKER =
-  process.env.MQTT_BROKER || config.mqtt.broker || 'mqtt://localhost:51883'
-const MQTT_USERNAME = process.env.MQTT_USERNAME || config.mqtt.username
-const MQTT_PASSWORD = process.env.MQTT_PASSWORD || config.mqtt.password
-
-DB.ensureTables()
-  .then(() => {
-    if (MQTT_USERNAME === undefined || MQTT_PASSWORD === undefined) {
-      console.error(
-        'Set MQTT_USERNAME and MQTT_PASSWORD env variables properly.'
-      )
-      process.exit(1)
-    } else {
-      startMqttClient(MQTT_BROKER, MQTT_USERNAME, MQTT_PASSWORD).then(
-        mqttClient => {
-          const writer = new SignalKDeltaWriter(DB)
-          const deltaInput = new MqttDeltaInput(mqttClient, writer)
-          deltaInput.start()
-        }
-      )
-    }
-  })
-  .catch(err => {
-    console.error(err)
-    process.exit(-1)
-  })
+import MqttRunner from './MqttRunner'
 
 process.on('unhandledRejection', error => {
   console.error('Unhandled promise exception:', error)
   process.exit(1)
 })
 
-function startMqttClient(
-  brokerUrl: string,
-  brokerUser: string,
-  brokerPw: string
-) {
-  const client = mqtt.connect(brokerUrl, {
-    username: brokerUser,
-    password: brokerPw
-  })
-  client.on('connect', () => console.log('Connected to MQTT server'))
-  client.on('offline', () => console.log('Disconnected from MQTT server'))
-  client.on('error', e => console.log('MQTT client error', e))
-
-  return BPromise.fromCallback(cb =>
-    client.once('connect', () => cb(null))
-  ).then(() => client)
-}
+// tslint:disable-next-line: no-unused-expression-chai
+new MqttRunner()
