0e10e1f61d431fea694b4d3a0e33e9aa3fbefd76
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-04-07 11:09:03 +0300

    Keep waiting even if action fails
    This is useful if we want to wait for some action to succeed instead of only asserting its return value

--- test/test-util.ts
+++ test/test-util.ts
@@ -31,13 +31,19 @@ export function waitFor<T>(
   action: () => Promise<T>,
   predicate: (t: T) => boolean
 ): Promise<T> {
-  return action().then(res => {
-    if (predicate(res)) {
-      return BPromise.resolve(res)
-    } else {
-      return BPromise.delay(100).then(() => waitFor(action, predicate))
-    }
-  })
+  return action()
+    .then(res => {
+      if (predicate(res)) {
+        return BPromise.resolve(res)
+      } else {
+        return retryAfterDelay()
+      }
+    })
+    .catch(retryAfterDelay)
+
+  function retryAfterDelay() {
+    return BPromise.delay(100).then(() => waitFor(action, predicate))
+  }
 }
 
 export function writeDeltasFromPositionFixture(): Promise<void> {
