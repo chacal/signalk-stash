442f5bdd85b46471c436cd26557d7208a208be66
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-04-07 11:09:59 +0300

    Wait for ClickHouse tables to really exist before returning from resetTables()
    This hopefully stabilises flaky tests in CI

--- test/TestDB.ts
+++ test/TestDB.ts
@@ -1,5 +1,9 @@
+import Debug from 'debug'
 import Config from '../api-server/Config'
 import StashDB from '../api-server/db/StashDB'
+import { waitFor } from './test-util'
+
+const debug = Debug('stash:TestDB')
 
 class TestDB {
   private readonly ch = StashDB.ch.ch
@@ -26,6 +30,26 @@ class TestDB {
       .then(() => this.ch.querying('DROP TABLE IF EXISTS trackpoint'))
       .then(() => this.ch.querying('DROP TABLE IF EXISTS value'))
       .then(() => StashDB.ensureTables())
+      .then(() => this.waitForClickHouseTables())
+  }
+
+  private waitForClickHouseTables() {
+    return Promise.all([
+      waitFor(
+        () => {
+          debug('Waiting for ClickHouse value trackpoint..')
+          return this.ch.querying('SELECT 1 FROM trackpoint')
+        },
+        () => true
+      ),
+      waitFor(
+        () => {
+          debug('Waiting for ClickHouse value table..')
+          return this.ch.querying('SELECT 1 FROM value')
+        },
+        () => true
+      )
+    ])
   }
 }
 
