10c85b4c9ec6d2b2b82b131ac723fc27b569094d
Teppo Kurki <teppo.kurki@iki.fi>
2019-06-24 10:10:11 +0300

    feature: update vessel insert sql
    Insert also stats acl and vessel table entry. Changes
    the parameters to <vesselid> <password> <vesselname>.

--- bin/generate_account_inserts.sh
+++ bin/generate_account_inserts.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 if [[ $# != 3 ]]; then
-    echo "Usage: $0 <username> <password> <topic>"
+    echo "Usage: $0 <vesselid> <password> <vesselname>"
     exit 1
 fi
 
-USERNAME=$1
+VESSELID=$1
 PASSWD=$2
-TOPIC=$3
+VESSELNAME=$3
 HASHED_PASSWD=$(docker run --rm jllopis/mosquitto np -p ${PASSWD})
 
-echo "INSERT INTO account (username, password, mosquitto_super) VALUES ('${USERNAME}', '${HASHED_PASSWD}', false);"
-echo "INSERT INTO mqtt_acl (account_id, topic, rw) VALUES ((SELECT id FROM account WHERE username = '${USERNAME}'), '${TOPIC}', 7);"
+echo "INSERT INTO account (username, password, mosquitto_super) VALUES ('${VESSELID}', '${HASHED_PASSWD}', false);"
+echo "INSERT INTO mqtt_acl (account_id, topic, rw) VALUES ((SELECT id FROM account WHERE username = '${VESSELID}'), 'signalk/delta/${VESSELID}', 7);"
+echo "INSERT INTO mqtt_acl (account_id, topic, rw) VALUES ((SELECT id FROM account WHERE username = '${VESSELID}'), 'signalk/delta/${VESSELID}/stats', 5);"
+echo "INSERT INTO vessel (name, vesselid) VALUES ('${VESSELNAME}','${VESSELID}');"
