9ef37a63e333bad65008a56c99c6888c53ef821c
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-09-15 13:20:07 +0300

    Parameterize coordinate resolution depending on zoom level
    Improve performance by using less accurate (and thus shorter) coordinates when extra accuracy is not needed

--- api-server/db/SKClickHouse.ts
+++ api-server/db/SKClickHouse.ts
@@ -141,6 +141,20 @@ export function timeResolutionForZoom(zoom: ZoomLevel) {
   }
 }
 
+export function coordinateDecimalsForZoom(zoom: ZoomLevel) {
+  if (zoom >= 18) {
+    return 6
+  } else if (zoom >= 14) {
+    return 5
+  } else if (zoom >= 11) {
+    return 4
+  } else if (zoom >= 9) {
+    return 3
+  } else {
+    return 3
+  }
+}
+
 export function simplifyThresholdForZoom(zoom?: ZoomLevel) {
   const maxThreshold = 0.003
   if (zoom !== undefined) {
