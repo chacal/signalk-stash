bb75ebd48a6ff18b0f80031667280bd65d5cd6e5
Teppo Kurki <teppo.kurki@iki.fi>
2019-10-01 21:45:19 +0300

    Add router & navigation

--- api-server/ui/App.tsx
+++ api-server/ui/App.tsx
@@ -1,13 +1,53 @@
+import { AppBar, Tab, Tabs } from '@material-ui/core'
 import * as React from 'react'
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+  withRouter
+} from 'react-router-dom'
 import { loadVessels } from './backend-requests'
 import MapPanel from './MapPanel'
 import TrackLengthsPanel from './TrackLengthsPanel'
 
-const App = () => {
-  if (document.location.hash === '#tl') {
-    return <TrackLengthsPanel />
-  } else {
-    return <MapPanel loadVessels={loadVessels} />
+const navigation = [
+  { label: 'Map', path: '/map' },
+  { label: 'Track lengths', path: '/tracklengths' }
+]
+
+const Navi = withRouter(props => {
+  const [value, setValue] = React.useState(0)
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    props.history.push(navigation[newValue].path)
+    setValue(newValue)
   }
+  return (
+    <AppBar position="static">
+      <Tabs value={value} onChange={handleChange}>
+        {navigation.map(({ label }) => (
+          <Tab label={label} />
+        ))}
+      </Tabs>
+    </AppBar>
+  )
+})
+
+const App = () => {
+  return (
+    <Router>
+      <Navi />
+      <Redirect exact from="/" to="map" />
+      <Switch>
+        <Route path="/map">
+          <MapPanel loadVessels={loadVessels} />
+        </Route>
+        <Route path="/tracklengths">
+          <TrackLengthsPanel />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
+
 export default App
