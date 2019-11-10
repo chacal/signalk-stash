aa726e4834d42d3b67c9afc918a52a342c2d9583
Teppo Kurki <teppo.kurki@iki.fi>
2019-11-10 18:42:39 +0200

    refactor: move leaflet css into Map.tsx

--- api-server/ui/Map.tsx
+++ api-server/ui/Map.tsx
@@ -30,47 +30,59 @@ const Map = ({ center, viewportA, tracksO }: MapProps) => {
   const updateBoundsFromEvent = (e: LeafletEvent) =>
     viewportA.set({ zoom: e.target.getZoom(), bounds: e.target.getBounds() })
 
+  const leafletCss = `
+  .leaflet-container {
+    height: 100%;
+    width: 100%;
+    margin: 0 auto;
+    }
+  `
   return (
-    <LeafletMap
-      center={center}
-      zoom={viewport.zoom}
-      onmoveend={updateBoundsFromEvent}
-      ref={updateBoundsFromMap}
-    >
-      <TileLayer
-        url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'}
-        minZoom={0}
-        maxZoom={20}
-      />
-      <TileLayer
-        url={'https://signalk-stash.chacal.fi/map/v1/{z}/{x}/{y}.png'}
-        minZoom={5}
-        maxZoom={15}
-      />
-      <TileLayer
-        url={'http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'}
-        minZoom={16}
-        maxNativeZoom={20}
-        maxZoom={21}
-        subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
-      />
-      <TileLayer
-        url={'https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png'}
-        minZoom={16}
-        maxZoom={21}
-        maxNativeZoom={18}
-      />
-      {tracks.map(track => (
-        // GeoJSON can't re-render itself when its props change ->
-        // use context + track load time as key to force re-render when new track
-        // for a vessel is loaded
-        <GeoJSON
-          key={keyFor(track)}
-          data={track.track}
-          color={track.color.hex()}
+    <span>
+      <style>{leafletCss}</style>
+      <LeafletMap
+        center={center}
+        zoom={viewport.zoom}
+        onmoveend={updateBoundsFromEvent}
+        ref={updateBoundsFromMap}
+        dragging={true}
+        touchZoom={true}
+      >
+        <TileLayer
+          url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'}
+          minZoom={0}
+          maxZoom={20}
         />
-      ))}
-    </LeafletMap>
+        <TileLayer
+          url={'https://signalk-stash.chacal.fi/map/v1/{z}/{x}/{y}.png'}
+          minZoom={5}
+          maxZoom={15}
+        />
+        <TileLayer
+          url={'http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'}
+          minZoom={16}
+          maxNativeZoom={20}
+          maxZoom={21}
+          subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
+        />
+        <TileLayer
+          url={'https://tiles.openseamap.org/seamark/{z}/{x}/{y}.png'}
+          minZoom={16}
+          maxZoom={21}
+          maxNativeZoom={18}
+        />
+        {tracks.map(track => (
+          // GeoJSON can't re-render itself when its props change ->
+          // use context + track load time as key to force re-render when new track
+          // for a vessel is loaded
+          <GeoJSON
+            key={keyFor(track)}
+            data={track.track}
+            color={track.color.hex()}
+          />
+        ))}
+      </LeafletMap>
+    </span>
   )
 }
 
