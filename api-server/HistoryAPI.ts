261427fea4b5f357fa852b53c51093b9243dc398
Teppo Kurki <teppo.kurki@iki.fi>
2020-01-13 21:00:40 +0200

    feature: add history paths api

--- api-server/HistoryAPI.ts
+++ api-server/HistoryAPI.ts
@@ -6,30 +6,27 @@ import { ZonedDateTime } from 'js-joda'
 import { asyncHandler } from './API'
 import SKClickHouse from './db/SKClickHouse'
 const contextsDebug = Debug('stash:history:contexts')
+const pathsDebug = Debug('stash:history:paths')
 
 const ch = new SKClickHouse().ch
 
 export default function setupHistoryAPIRoutes(app: Express) {
-  app.get('/signalk/v1/history/contexts', asyncHandler(contexts))
-}
-
-async function contexts(req: Request, res: Response) {
-  contextsDebug(req.query)
-  const from = dateTimeFromQuery(req, 'from')
-  const to = dateTimeFromQuery(req, 'to')
-  contextsDebug(`${from.toString()}-${to.toString()}`)
-  res.json(await getContexts(ch, from, to))
-}
-
-function dateTimeFromQuery(req: Request, paramName: string): ZonedDateTime {
-  return ZonedDateTime.parse(req.query[paramName])
+  app.get(
+    '/signalk/v1/history/contexts',
+    asyncHandler(fromToHandler(getContexts, contextsDebug))
+  )
+  app.get(
+    '/signalk/v1/history/paths',
+    asyncHandler(fromToHandler(getPaths, pathsDebug))
+  )
 }
 
 type ContextResultRow = [string]
 async function getContexts(
   ch: Clickhouse,
   from: ZonedDateTime,
-  to: ZonedDateTime
+  to: ZonedDateTime,
+  debug: (s: string) => void
 ) {
   const coreQuery = ['value', 'trackpoint']
     .map(
@@ -45,8 +42,59 @@ async function getContexts(
     )
     .join(' UNION ALL ')
   const distinctQuery = `SELECT DISTINCT context from (${coreQuery})`
-  contextsDebug(distinctQuery)
+  debug(distinctQuery)
   return ch
     .querying<ContextResultRow>(distinctQuery)
     .then((result: any) => result.data.map((row: any[]) => row[0]))
+}
+
+type PathsResultRow = [string]
+async function getPaths(
+  ch: Clickhouse,
+  from: ZonedDateTime,
+  to: ZonedDateTime,
+  debug: (s: string) => void,
+  req: Request
+) {
+  const context = req.query.context || ''
+  const query = `
+      SELECT
+        DISTINCT path
+      FROM value
+      WHERE
+        context = '${context}'
+        AND
+        ts >= ${from.toEpochSecond()}
+        AND
+        ts <= ${to.toEpochSecond()}
+    `
+  debug(query)
+  return ch
+    .querying<PathsResultRow>(query)
+    .then((result: any) => result.data.map((row: any[]) => row[0]))
+}
+
+type FromToHandler<T = any> = (
+  ch: Clickhouse,
+  from: ZonedDateTime,
+  to: ZonedDateTime,
+  debug: (d: string) => void,
+  req: Request
+) => Promise<T>
+
+function fromToHandler(
+  wrappedHandler: FromToHandler,
+  debug: (d: string) => void
+) {
+  return async (req: Request, res: Response) => {
+    debug(req.query)
+    const from = dateTimeFromQuery(req, 'from')
+    const to = dateTimeFromQuery(req, 'to')
+    contextsDebug(`${from.toString()}-${to.toString()}`)
+    res.json(await wrappedHandler(ch, from, to, debug, req))
+  }
+}
+
+function dateTimeFromQuery(req: Request, paramName: string): ZonedDateTime {
+  return ZonedDateTime.parse(req.query[paramName])
 }
