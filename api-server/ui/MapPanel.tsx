8ffbb6000f8b39890ce07ce0b3477e1d94ac4fa7
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2021-06-13 22:34:30 +0300

    Add sliders to change satellite imagery brightness & contrast

--- api-server/ui/MapPanel.tsx
+++ api-server/ui/MapPanel.tsx
@@ -1,8 +1,9 @@
-import _ from 'lodash'
 import * as React from 'react'
 
+import { map } from 'rxjs/operators'
 import Map from './Map'
 import { MapPanelState } from './mappanel-state'
+import MapVisibilityAdjustmentPanel from './MapVisibilityAdjustmentPanel'
 import TimeSelectionPanel from './TimeSelectionPanel'
 import { VesselSelectionState } from './vesselselection-state'
 import VesselSelectionPanel from './VesselSelectionPanel'
@@ -23,6 +24,9 @@ const MapPanel = ({ vesselSelection }: MapPanelProps) => {
       />
       <VesselSelectionPanel selectionState={panelState.vesselSelectionState} />
       <TimeSelectionPanel timeselection={panelState.timeSelectionState} />
+      <MapVisibilityAdjustmentPanel
+        zoom={panelState.viewport.pipe(map(v => v.zoom))}
+      />
     </React.Fragment>
   )
 }
