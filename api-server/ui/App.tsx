e3c4378a18be4ca354ba00e1f4febd71b3f7bed9
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2021-07-24 10:04:26 +0300

    Require auth0 login in UI

--- api-server/ui/App.tsx
+++ api-server/ui/App.tsx
@@ -1,4 +1,13 @@
-import { AppBar, Tab, Tabs } from '@material-ui/core'
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react'
+import {
+  AppBar,
+  Button,
+  Grid,
+  Hidden,
+  Tab,
+  Tabs,
+  Typography
+} from '@material-ui/core'
 import * as React from 'react'
 import { useEffect } from 'react'
 import {
@@ -32,13 +41,44 @@ const Navi = withRouter(({ location, history }) => {
     history.push(navigation[newValue].path)
     setTabIndex(newValue)
   }
+
+  const { logout, user } = useAuth0()
+
   return (
     <AppBar position="static">
-      <Tabs value={tabIndex} onChange={handleChange}>
-        {navigation.map(({ label }) => (
-          <Tab key={label} label={label} />
-        ))}
-      </Tabs>
+      <Grid container spacing={0} alignItems={'center'}>
+        <Grid item xs={8}>
+          <Tabs value={tabIndex} onChange={handleChange}>
+            {navigation.map(({ label }) => (
+              <Tab key={label} label={label} />
+            ))}
+          </Tabs>
+        </Grid>
+        <Grid
+          container
+          item
+          xs={4}
+          justify={'flex-end'}
+          alignItems={'center'}
+          spacing={3}
+        >
+          <Hidden smDown>
+            <Grid item>
+              <Typography variant={'overline'}>
+                {user !== undefined ? user.email : ''}
+              </Typography>
+            </Grid>
+          </Hidden>
+          <Grid item>
+            <Button
+              color="inherit"
+              onClick={() => logout({ returnTo: window.location.origin })}
+            >
+              Logout
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
     </AppBar>
   )
 })
@@ -78,4 +118,4 @@ const App = () => {
   )
 }
 
-export default App
+export default withAuthenticationRequired(App)
--- api-server/ui/index.tsx
+++ api-server/ui/index.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
+import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App'
 import './main.less'
 
 // Render main component
-ReactDOM.render(<App />, document.getElementById('main'))
+ReactDOM.render(
+  <Auth0Provider
+    domain="signalk-stash.eu.auth0.com"
+    clientId="BPqbNlJWgvMR2ZxuvVj1dGm0pVKYxb2a"
+    redirectUri={window.location.origin}
+    audience="https://signalk-stash.chacal.fi"
+    scope="read:signalk_stash"
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('main')
+)
