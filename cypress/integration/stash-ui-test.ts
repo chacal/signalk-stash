e9d5b5f1caf2e286ef582fdf31cee3c234248242
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2021-07-31 20:03:30 +0300

    Increase Cypress test response timeout from 30s to 60s
    Give more time for test initialization calls in before() block

--- cypress/integration/stash-ui-test.ts
+++ cypress/integration/stash-ui-test.ts
@@ -1,48 +1,55 @@
-describe('Stash UI', () => {
-  before(() => {
-    cy.request('POST', '/test/reset-tables')
-    cy.request('POST', '/test/insert-positions')
-  })
+describe(
+  'Stash UI',
+  {
+    responseTimeout: 60000
+  },
+  () => {
+    before(() => {
+      cy.request('POST', '/test/reset-tables')
+      cy.request('POST', '/test/insert-positions')
+    })
 
-  it('renders vessel list', () => {
-    cy.login()
-    cy.visit('/')
+    it('renders vessel list', () => {
+      cy.login()
+      cy.visit('/')
 
-    vesselItems().should('have.length', 2)
+      vesselItems().should('have.length', 2)
 
-    vesselItem(0).should('not.be.visible')
-    vesselItem(1).should('not.be.visible')
+      vesselItem(0).should('not.be.visible')
+      vesselItem(1).should('not.be.visible')
 
-    vesselToggle().click()
+      vesselToggle().click()
 
-    vesselItem(0).should('be.visible')
-    vesselItem(1).should('be.visible')
+      vesselItem(0).should('be.visible')
+      vesselItem(1).should('be.visible')
 
-    checkboxContainers().should($items =>
-      expect($items.get(0).style.color).to.not.equal($items.get(1).style.color)
-    )
+      checkboxContainers().should($items =>
+        expect($items.get(0).style.color).to.not.equal(
+          $items.get(1).style.color
+        )
+      )
 
-    vesselItem(0).contains('Test Vessel')
-    vesselItem(1).contains('foo')
-    checkbox(0).should('not.be.checked')
-    checkbox(1).should('not.be.checked')
+      vesselItem(0).contains('Test Vessel')
+      vesselItem(1).contains('foo')
+      checkbox(0).should('not.be.checked')
+      checkbox(1).should('not.be.checked')
 
-    checkbox(0).click()
+      checkbox(0).click()
 
-    checkbox(0).should('be.checked')
-    checkbox(1).should('not.be.checked')
+      checkbox(0).should('be.checked')
+      checkbox(1).should('not.be.checked')
 
-    vesselItem(0).click()
+      vesselItem(0).click()
 
-    checkbox(0).should('not.be.checked')
-    checkbox(1).should('not.be.checked')
-  })
-})
+      checkbox(0).should('not.be.checked')
+      checkbox(1).should('not.be.checked')
+    })
+  }
+)
 
 function vesselToggle() {
   return cy.get('[data-cy=vessel-selection-panel-caret]')
 }
-
 
 function vesselItems() {
   return cy.get('[data-cy=vessel-selection-panel__vessel]')
