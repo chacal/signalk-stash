f9f33840bd5952a9794a1d1b164362e49e1a41e3
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2021-08-08 22:15:09 +0300

    Increase Cypress request timeout to give CI enough time to insert positions fixture

--- cypress/integration/stash-ui-test.ts
+++ cypress/integration/stash-ui-test.ts
@@ -1,49 +1,57 @@
-describe('Stash UI', () => {
-  before(() => {
-    cy.request('POST', '/test/reset-tables')
-    cy.request('POST', '/test/insert-positions')
-    cy.request('POST', '/test/login')
-    cy.visit('/')
-  })
+describe(
+  'Stash UI',
+  {
+    responseTimeout: 60000
+  },
+  () => {
+    before(() => {
+      cy.request('POST', '/test/reset-tables')
+      cy.request('POST', '/test/insert-positions')
+      cy.request('POST', '/test/login')
+      cy.visit('/')
+    })
 
-  it('renders vessel list', () => {
-    vesselItems().should('have.length', 2)
+    it('renders vessel list', () => {
+      vesselItems().should('have.length', 2)
 
-    vesselItem(0).should('not.be.visible')
-    vesselItem(1).should('not.be.visible')
+      vesselItem(0).should('not.be.visible')
+      vesselItem(1).should('not.be.visible')
 
-    vesselToggle().click()
+      vesselToggle().click()
 
-    vesselItem(0).should('be.visible')
-    vesselItem(1).should('be.visible')
+      vesselItem(0).should('be.visible')
+      vesselItem(1).should('be.visible')
 
-    checkboxContainers().should($items =>
-      expect($items.get(0).style.color).to.not.equal($items.get(1).style.color)
-    )
+      checkboxContainers().should($items =>
+        expect($items.get(0).style.color).to.not.equal(
+          $items.get(1).style.color
+        )
+      )
 
-    vesselItem(0).contains('bar')
-    vesselItem(1).contains('foo')
-    checkbox(0).should('not.be.checked')
-    checkbox(1).should('not.be.checked')
+      vesselItem(0).contains('bar')
+      vesselItem(1).contains('foo')
+      checkbox(0).should('not.be.checked')
+      checkbox(1).should('not.be.checked')
 
-    checkbox(0).click()
+      checkbox(0).click()
 
-    checkbox(0).should('be.checked')
-    checkbox(1).should('not.be.checked')
+      checkbox(0).should('be.checked')
+      checkbox(1).should('not.be.checked')
 
-    vesselItem(0).click()
+      vesselItem(0).click()
 
-    checkbox(0).should('not.be.checked')
-    checkbox(1).should('not.be.checked')
-  })
+      checkbox(0).should('not.be.checked')
+      checkbox(1).should('not.be.checked')
+    })
 
-  it('shows logged in email', () => {
-    cy.get('[data-cy=account-toolbar__email]').should(
-      'have.text',
-      'unittest@signalk-stash-dev.chacal.fi'
-    )
-  })
-})
+    it('shows logged in email', () => {
+      cy.get('[data-cy=account-toolbar__email]').should(
+        'have.text',
+        'unittest@signalk-stash-dev.chacal.fi'
+      )
+    })
+  }
+)
 
 function vesselToggle() {
   return cy.get('[data-cy=vessel-selection-panel-caret]')
