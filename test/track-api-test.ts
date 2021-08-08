3ee18b97457bb8c8c3f781fd9ccef2a7ae20454e
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2021-08-08 10:03:40 +0300

    Use auth in API unit tests

--- test/track-api-test.ts
+++ test/track-api-test.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai'
 import {
   assertCoords,
   assertValidationErrors,
+  getAuthorizedJson,
   getJson,
   startTestApp,
   testVesselUuids,
@@ -14,8 +15,12 @@ describe('Track API', () => {
 
   beforeEach(() => TestDB.resetTables().then(writeDeltasFromPositionFixture))
 
+  it('requires authentication', () => {
+    return getJson(app, '/tracks', 401)
+  })
+
   it('returns tracks for self vessel', () => {
-    return getJson(app, '/tracks')
+    return getAuthorizedJson(app, '/tracks')
       .query({ context: testVesselUuids[2] })
       .expect(res => {
         expect(res.body.type).to.equal('MultiLineString')
@@ -25,7 +30,7 @@ describe('Track API', () => {
   })
 
   it('returns track for timespan', () => {
-    return getJson(app, '/tracks')
+    return getAuthorizedJson(app, '/tracks')
       .query({
         context: testVesselUuids[0],
         from: '2014-08-15T19:00:22',
@@ -39,7 +44,7 @@ describe('Track API', () => {
   })
 
   it('returns multiple tracks for a timespan', () => {
-    return getJson(app, '/tracks')
+    return getAuthorizedJson(app, '/tracks')
       .query({
         context: testVesselUuids[0],
         from: '2014-08-15T19:00:22',
@@ -52,7 +57,7 @@ describe('Track API', () => {
   })
 
   it('returns no tracks for a timespan', () => {
-    return getJson(app, '/tracks')
+    return getAuthorizedJson(app, '/tracks')
       .query({
         context: testVesselUuids[0],
         from: '2012-08-15T19:00:22',
@@ -65,7 +70,7 @@ describe('Track API', () => {
   })
 
   it('invalid timespan returns error', () => {
-    return getJson(app, '/tracks', 400).query({
+    return getAuthorizedJson(app, '/tracks', 400).query({
       context: testVesselUuids[0],
       from: '12-99-77T19:00:22',
       to: '2013-08-15T19:01:25'
@@ -73,7 +78,7 @@ describe('Track API', () => {
   })
 
   it('returns tracks with bounding box', () => {
-    return getJson(app, '/tracks')
+    return getAuthorizedJson(app, '/tracks')
       .query({
         context: testVesselUuids[2],
         s: 59,
@@ -90,7 +95,7 @@ describe('Track API', () => {
       })
   })
   it('returns empty track with bounding box', () => {
-    return getJson(app, '/tracks')
+    return getAuthorizedJson(app, '/tracks')
       .query({
         context: testVesselUuids[2],
         s: 40,
@@ -104,12 +109,12 @@ describe('Track API', () => {
       })
   })
   it('returns error if context is missing', () => {
-    return getJson(app, '/tracks', 400).expect(res =>
+    return getAuthorizedJson(app, '/tracks', 400).expect(res =>
       assertValidationErrors(res, '"context" is required')
     )
   })
   it('returns error if bounding box is invalid', () => {
-    return getJson(app, '/tracks', 400)
+    return getAuthorizedJson(app, '/tracks', 400)
       .query({ context: testVesselUuids[2], n: 59.5, s: 'abcdefg', e: 1500 })
       .expect(res =>
         assertValidationErrors(
@@ -121,7 +126,7 @@ describe('Track API', () => {
       )
   })
   it('returns track with zoom level 20', () => {
-    return getJson(app, '/tracks')
+    return getAuthorizedJson(app, '/tracks')
       .query({
         context: testVesselUuids[2],
         zoomLevel: 20 // Use 2s time interval -> will return all 3 points
@@ -132,7 +137,7 @@ describe('Track API', () => {
       })
   })
   it('returns track with zoom level 11', () => {
-    return getJson(app, '/tracks')
+    return getAuthorizedJson(app, '/tracks')
       .query({
         context: testVesselUuids[2],
         zoomLevel: 11 // Use 30s time interval -> will return only 2 points (two first are averaged)
@@ -145,14 +150,14 @@ describe('Track API', () => {
       })
   })
   it('returns error with invalid zoom level', () => {
-    return getJson(app, '/tracks', 400)
+    return getAuthorizedJson(app, '/tracks', 400)
       .query({ context: testVesselUuids[2], zoomLevel: 'test' })
       .expect(res =>
         assertValidationErrors(res, '"zoomLevel" must be a number')
       )
   })
   it('returns error with too small zoom level', () => {
-    return getJson(app, '/tracks', 400)
+    return getAuthorizedJson(app, '/tracks', 400)
       .query({ context: testVesselUuids[2], zoomLevel: 0 })
       .expect(res =>
         assertValidationErrors(res, '"zoomLevel" must be greater than 0')
