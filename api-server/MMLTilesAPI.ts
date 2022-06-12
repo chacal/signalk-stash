322928d4cdab645244bfc015c7997c0f6aa30b92
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2022-06-12 10:14:28 +0300

    Use Kapsi as aerial image provider
    NLS (Maanmittauslaitos) directly provides only coarser images from their open API, but Kapsi has the higher resolution ones available.

--- api-server/MMLTilesAPI.ts
+++ api-server/MMLTilesAPI.ts
@@ -4,7 +4,7 @@ import { Express, Request, Response } from 'express'
 export default function setupMMLTilesAPIRoutes(app: Express) {
   app.get('/ortokuva/:z/:x/:y.jpg', async (req, res) => {
     const { z, x, y } = req.params
-    const url = `https://karttamoottori.maanmittauslaitos.fi/maasto/wmts/1.0.0/ortokuva/default/WGS84_Pseudo-Mercator/${z}/${y}/${x}.jpg`
+    const url = `https://tiles.kartat.kapsi.fi/ortokuva/${z}/${x}/${y}.jpg`
     await proxyRequest(url, req, res)
   })
   app.get('/maastokartta/:z/:x/:y.jpg', async (req, res) => {
